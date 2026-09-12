33e2ea46dd8503f3f4d4e6882184e7d9a7ffd4ca
shashi kiran <sunnyshashikiran0@gmail.com>
2026-09-12 22:46:09 +0530

    Prepare backend for cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const db = require("./config/database");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
